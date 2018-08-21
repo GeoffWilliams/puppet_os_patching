8868e0dd45e1d900bcea42e89b04c11242ba30c3
Tony Green <tgreen@bandcamp.tv>
2018-08-22 09:22:34 +1000

    initial windows effort

--- manifests/init.pp
+++ manifests/init.pp
@@ -171,7 +171,7 @@ class os_patching (
       ensure  => file,
       content => $patch_window,
       require => File[$cache_dir],
-      notify  => Exec[$fact_upload],
+      notify  => Exec['Fact upload'],
     }
   } else {
     file { $patch_window_file:
@@ -222,7 +222,7 @@ class os_patching (
       ensure  => file,
       content => template("${module_name}/blackout_windows.erb"),
       require => File[$cache_dir],
-      notify  => Exec[$fact_upload],
+      notify  => Exec['Fact upload'],
     }
   } else {
     file { $blackout_window_file:
