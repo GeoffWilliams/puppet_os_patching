338208a9d6ce46d30d900c7b12a37a177ef97a98
Tony Green <tgreen@bandcamp.tv>
2018-08-28 12:29:37 +1000

    clean up patch server for windows

--- tasks/patch_server_win.rb
+++ tasks/patch_server_win.rb
@@ -22,6 +22,18 @@ begin
     _fact_out, _stderr, _status = Open3.capture3('powershell C:/ProgramData/PuppetLabs/puppet/cache/os_patching_fact_generation.ps1')
     exit 1
   end
+  cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -WindowsUpdate -AcceptAll #{_allow_reboot}\""
+  stdout, stderr, status = Open3.capture3(cmd_string)
+  if status == 0
+    puts stdout.strip
+    exit 0
+  else
+    puts 'Could not apply patch'
+    exit (-1)
+  end
+  _fact_out, stderr, status = Open3.capture3('C:/ProgramData/PuppetLabs/puppet/cache/os_patching_fact_generation.ps1')
+  err(status, 'os_patching/fact', stderr, starttime) if status != 0
 rescue StandardError => e
   raise "There was a problem #{e.message}"
+  exit (-1)
 end
