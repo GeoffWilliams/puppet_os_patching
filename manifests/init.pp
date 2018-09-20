5a0703508955759cbe83df69f6464a4fda4d723e
Tony Green <tgreen@bandcamp.tv>
2018-09-21 09:26:50 +1000

    Manual merge of dev code

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,9 @@
 # @param [String] patch_data_group Group name for the owner of the patch data
 # @param [String] patch_cron_user User name to run the cron job as (defaults to patch_data_owner)
 # @param [Boolean] install_delta_rpm Should the deltarpm package be installed on RedHat family nodes
-# @param [Boolean] reboot_override Controls on a node level if a reboot should/should not be done after patching.
+# lint:ignore:140chars
+# @param Variant[Boolean, Enum['always', 'never', 'patched', 'smart', 'default']] reboot_override Controls on a node level if a reboot should/should not be done after patching.
+# lint:endignore
 #		This overrides the setting in the task
 # @param [Hash] blackout_windows A hash containing the patch blackout windows, which prevent patching.
 #   The dates are in full ISO8601 format.
@@ -19,7 +21,7 @@
 # @example assign node to 'Week3' patching window, force a reboot and create a blackout window for the end of the year
 #   class { 'os_patching':
 #     patch_window     => 'Week3',
-#     reboot_override  => true,
+#     reboot_override  => 'always',
 #     blackout_windows => { 'End of year change freeze':
 #       {
 #         'start': '2018-12-15T00:00:00+1000',
@@ -32,7 +34,7 @@
 #   class profiles::soe::patching (
 #     $patch_window     = undef,
 #     $blackout_windows = undef,
-#     $reboot_override  = undef,
+#     $reboot_override  = 'default',
 #   ){
 #     # Pull any blackout windows out of hiera
 #     $hiera_blackout_windows = lookup('profiles::soe::patching::blackout_windows',Hash,hash,{})
@@ -56,7 +58,7 @@ class os_patching (
   String $patch_data_group           = 'root',
   String $patch_cron_user            = $patch_data_owner,
   Boolean $install_delta_rpm         = false,
-  Optional[Boolean] $reboot_override = undef,
+  Optional[Variant[Boolean, Enum['always', 'never', 'patched', 'smart', 'default']]] $reboot_override = 'default',
   Optional[Hash] $blackout_windows   = undef,
   $patch_window                      = undef,
   $patch_cron_hour                   = absent,
@@ -182,11 +184,9 @@ class os_patching (
   $reboot_override_file = "${cache_dir}/reboot_override"
   if ( $reboot_override != undef ) {
     case $reboot_override {
-      # lint:ignore:quoted_booleans
-      true:  { $reboot_boolean = 'true' }
-      false: { $reboot_boolean = 'false' }
-      default: { fail ('reboot_override must be a boolean')}
-      # lint:endignore
+      true:     { $reboot_override_value = 'always' }
+      false:    { $reboot_override_value = 'never' }
+      default:  { $reboot_override_value = $reboot_override }
     }
 
     file { $reboot_override_file:
